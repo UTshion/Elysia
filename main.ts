5712f344f038eb7fad64145c4a732e0d6d6cb959
UTshion <yannbarushion@gmail.com>
2024-06-21 22:55:30 +0900

    first build of the plugin

--- main.ts
+++ main.ts
@@ -1,134 +1,157 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Modal, Notice, Plugin, PluginSettingTab, Setting, addIcon, MarkdownView } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface EnglishWordData {
+    word: string;
+    meaning: string;
+    example: string;
+    example_translation: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+export default class EnglishLearningPlugin extends Plugin {
+    async onload() {
+        console.log('loading English Learning plugin');
+
+		addIcon('english-word', `<svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-width="1.5" width="100" height="100" color="#000000"><defs><style>.cls-6374f8d9b67f094e4896c628-1{fill:none;stroke:currentColor;stroke-miterlimit:10;}</style></defs><rect class="cls-6374f8d9b67f094e4896c628-1" x="3.39" y="1.5" width="19.09" height="21"></rect><circle class="cls-6374f8d9b67f094e4896c628-1" cx="13.89" cy="8.18" r="2.86"></circle><path class="cls-6374f8d9b67f094e4896c628-1" d="M9.11,15.82l.21-1a4.65,4.65,0,0,1,4.57-3.74h0a4.64,4.64,0,0,1,4.56,3.74l.21,1"></path><line class="cls-6374f8d9b67f094e4896c628-1" x1="0.52" y1="5.32" x2="6.25" y2="5.32"></line><line class="cls-6374f8d9b67f094e4896c628-1" x1="0.52" y1="10.09" x2="6.25" y2="10.09"></line><line class="cls-6374f8d9b67f094e4896c628-1" x1="0.52" y1="14.86" x2="6.25" y2="14.86"></line><line class="cls-6374f8d9b67f094e4896c628-1" x1="0.52" y1="19.64" x2="6.25" y2="19.64"></line></svg>`);
+
+        // add ribbon icon
+        this.addRibbonIcon('english-word', 'Add English Word', this.addEnglishWord.bind(this));
+
+        this.addCommand({
+            id: 'add-english-word',
+            name: 'Add English Word',
+            callback: this.addEnglishWord.bind(this)
+        });
+
+        this.addSettingTab(new EnglishLearningSettingTab(this.app, this));
+    }
+
+    onunload() {
+        console.log('unloading English Learning plugin');
+    }
+
+    async addEnglishWord() {
+        const wordData = await this.getWordDataFromUser();
+        if (wordData) {
+            const formattedContent = this.formatWordData(wordData);
+            await this.appendToCurrentNote(formattedContent);
+            new Notice('English Word has been added');
+        }
+    }
+
+    async getWordDataFromUser(): Promise<EnglishWordData | null> {
+        return new Promise((resolve) => {
+            const modal = new EnglishWordModal(this.app, (result) => resolve(result));
+            modal.open();
+        });
+    }
+
+    formatWordData(data: EnglishWordData): string {
+        return `
+## ${data.word}
+${data.meaning}
+
+---
+
+${data.example}
+${data.example_translation}
+`;
+    }
+
+    async appendToCurrentNote(content: string) {
+        const currentView = this.app.workspace.getActiveViewOfType(MarkdownView);
+        if (currentView) {
+            const file = currentView.file;
+            if (file) {
+                const currentContent = await this.app.vault.read(file);
+                await this.app.vault.modify(file, currentContent + '\n' + content);
+            } else {
+                new Notice('The current view is not associated with a file');
+            }
+        } else {
+            new Notice('There is no active view');
+        }
+    }
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+class EnglishWordModal extends Modal {
+    result: EnglishWordData;
+    onSubmit: (result: EnglishWordData) => void;
 
-	async onload() {
-		await this.loadSettings();
+    constructor(app: App, onSubmit: (result: EnglishWordData) => void) {
+        super(app);
+        this.onSubmit = onSubmit;
+        this.result = { word: '', meaning: '', example: '', example_translation: '' };
+    }
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+    onOpen() {
+        const { contentEl } = this;
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+        contentEl.createEl("h1", { text: "Add English Word" });
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+        new Setting(contentEl)
+            .setName("English Word")
+            .addText(text => text
+                .setPlaceholder("Enter English word")
+                .onChange(value => this.result.word = value));
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+        new Setting(contentEl)
+            .setName("Meaning in Japanese")
+            .addText(text => text
+                .setPlaceholder("Enter Japanese meaning")
+                .onChange(value => this.result.meaning = value));
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+        new Setting(contentEl)
+            .setName("Some Example Text (English)")
+            .addText(text => text
+                .setPlaceholder("Enter English example")
+                .onChange(value => this.result.example = value));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+        new Setting(contentEl)
+            .setName("Some Example Text (Japanese)")
+            .addText(text => text
+                .setPlaceholder("Enter Japanese translation")
+                .onChange(value => this.result.example_translation = value));
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
+        new Setting(contentEl)
+            .addButton(btn => btn
+                .setButtonText("Add")
+                .setCta()
+                .onClick(() => {
+                    this.close();
+                    this.onSubmit(this.result);
+                }));
+    }
 
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+    onClose() {
+        const { contentEl } = this;
+        contentEl.empty();
+    }
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class EnglishLearningSettingTab extends PluginSettingTab {
+    plugin: EnglishLearningPlugin;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+    constructor(app: App, plugin: EnglishLearningPlugin) {
+        super(app, plugin);
+        this.plugin = plugin;
+    }
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
+    display(): void {
+        const {containerEl} = this;
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+        containerEl.empty();
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+        containerEl.createEl('h2', {text: 'English Learning Plugin Settings'});
 
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+        new Setting(containerEl)
+            .setName('Example Setting')
+            .setDesc('This is an example setting.')
+            .addText(text => text
+                .setPlaceholder('Enter your setting')
+                .setValue('')
+                .onChange(async (value) => {
+                    console.log('Example setting: ' + value);
+                    await this.plugin.saveData({exampleSetting: value});
+                }));
+    }
 }
